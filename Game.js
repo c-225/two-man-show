c806706345921ad1c4b363f8778c0a6d81c5a122
c-225 <calvin.zegbeu@gmail.com>
2024-12-15 03:38:40 +0100

    debut canvas caché

--- Game.js
+++ Game.js
@@ -1,12 +1,41 @@
 import Player from './Classes/Player.js';
 import Obstacle from './Classes/Obstacle.js';
 
+let joueurs = []
+let controls = []
+let keys = ["KeyW", "KeyT", "KeyO", "ArrowUp"]
+let state = "init"
+
+document.addEventListener('keyup', (e) => {
+    if (state==="init") {
+        if (keys.includes(e.code)) {
+            let indice = keys.indexOf(e.code);
+            keys[indice]=null
+            joueurs.push("J"+(1+joueurs.length))
+
+            if (e.code === "KeyW"){ controls.push( [ "KeyW", "KeyS", "KeyA", "KeyD", { KeyW : false, KeyS : false, KeyA : false, KeyD : false}] ) }
+            if (e.code === "KeyT"){ controls.push( [ "KeyT", "KeyG", "KeyF", "KeyH", { KeyT : false, KeyG : false, KeyF : false, KeyH : false}] ) }
+            if (e.code === "KeyO"){ controls.push( [ "KeyO", "KeyL", "KeyK", "Semicolon", { KeyO : false, KeyL : false, KeyK : false, Semicolon : false}] ) }
+            if (e.code === "ArrowUp"){ controls.push( [ "ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight", { ArrowUp : false, ArrowDown : false, ArrowLeft : false, ArrowRight : false}] ) }
+            var paragraph = document.getElementById("listeP");
+            paragraph.innerHTML += joueurs[joueurs.length-1] +" est pret ! <br/>"
+            if (joueurs.length===4) {
+                var canvas = document.getElementById("gameCanvas")
+                canvas.style.display='block'
+                var init = document.getElementById("intro")
+            console.log(init)
+                init.style.display='none'
+            }
+        }
+    }
+})
+
 const canvas = document.getElementById('gameCanvas');
 const context = canvas.getContext('2d');
 
 const players = [
     new Player(50, 50, 30, 5, 'green', ['KeyW','KeyS','KeyA','KeyD', { KeyW: false, KeyS: false, KeyA: false, KeyD: false }]),
-    new Player(0, 0, 30, 5, 'blue',[ 'ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight',{ 'ArrowUp': false, 'ArrowDown': false, 'ArrowLeft': false, 'ArrowRight': false }]),
+    new Player(0, 0, 30, 5, 'blue',[ 'ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight',{ ArrowUp: false, ArrowDown: false, ArrowLeft: false, ArrowRight: false }]),
     new Player(100, 100, 30, 5, 'orange', [ 'KeyT','KeyG','KeyF','KeyH',{ KeyT: false, KeyG: false, KeyF: false, KeyH: false }]),
     new Player(150, 150, 30, 5, 'purple', [ 'KeyO','KeyL','KeyK','Semicolon',{ KeyO: false, KeyL: false, KeyK: false, Semicolon: false }]),
 ];
---
+++ moteur.js
@@ -0,0 +1,25 @@
+//import Player from "./Classes/Player"
+let joueurs = []
+let controls = []
+let keys = ["KeyW", "KeyT", "KeyO", "ArrowUp"]
+let state = "init"
+
+document.addEventListener('keyup', (e) => {
+document.getElementById("Play").disabled = true;
+    if (state==="init") {
+        if (keys.includes(e.code)) {
+            let indice = keys.indexOf(e.code);
+            keys[indice]=null
+            joueurs.push("J"+(1+joueurs.length))
+
+            if (e.code === "KeyW"){ controls.push( [ "KeyW", "KeyS", "KeyA", "KeyD", { KeyW : false, KeyS : false, KeyA : false, KeyD : false}] ) }
+            if (e.code === "KeyT"){ controls.push( [ "KeyT", "KeyG", "KeyF", "KeyH", { KeyT : false, KeyG : false, KeyF : false, KeyH : false}] ) }
+            if (e.code === "KeyO"){ controls.push( [ "KeyO", "KeyL", "KeyK", "Semicolon", { KeyO : false, KeyL : false, KeyK : false, Semicolon : false}] ) }
+            if (e.code === "ArrowUp"){ controls.push( [ "ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight", { ArrowUp : false, ArrowDown : false, ArrowLeft : false, ArrowRight : false}] ) }
+            var paragraph = document.getElementById("listeP");
+            paragraph.innerHTML += joueurs[joueurs.length-1] +" est pret ! <br/>"
+            document.getElementById("Play").disabled = false;
+            console.log(controls[controls.length-1][4])
+        }
+    }
+})
